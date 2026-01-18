e798073f18236b353f2c73d7efe6d4e292b09f4e
Raj602-stack <71328233+Raj602-stack@users.noreply.github.com>
2026-01-19 01:19:01 +0530

    api url changed

--- src/services/carsApi.js
+++ src/services/carsApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api"; // change this
+const API_BASE_URL = "https://api.carsdedo.com/api"; // change this
 
 export const fetchCars = async () => {
   const response = await axios.get(`${API_BASE_URL}/cars/`);
